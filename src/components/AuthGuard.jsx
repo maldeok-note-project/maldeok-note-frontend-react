0242fe321ebc983fd7406f57ba91da9bde81c2a0
Azuki.I <azuking69@hotmail.co.jp>
2026-06-16 22:56:07 +0900

    26.06.16 fixed codes AuthGuard.jsx

--- src/components/AuthGuard.jsx
+++ src/components/AuthGuard.jsx
@@ -3,7 +3,7 @@ import { Navigate, replace } from "react-router-dom";
 // 未ログイン者を弾く
 const AuthGuard = ({ children }) => {
   // ストレージからトークン取得
-  const token = localStorage.setItem("token");
+  const token = localStorage.getItem("token");
 
   // トークンが無ければログイン画面へ
   if (!token) {
